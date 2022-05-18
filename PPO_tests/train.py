469c8c58b314ac23546ee1a2e81be91bcea1f4eb
Gaëtan Serré <gaetan.serre@universite-paris-saclay.fr>
2022-05-18 23:05:02 +0200

    CarRacing-V0 & Tanh

--- PPO_tests/train.py
+++ PPO_tests/train.py
@@ -3,12 +3,12 @@ from stable_baselines3.common.env_util import make_vec_env
 import torch.nn as nn
 
 archs = [[1], [10], [64, 64], [128, 128], [256, 64]]
-envs = ["Pendulum-v1", "CarRacing-v1", "MountainCarContinuous-v0"]
+envs = ["Pendulum-v1", "CarRacing-v0", "MountainCarContinuous-v0"]
 names = ["pendulum", "car_racing", "moutaincar_c0"]
 
 for arch in archs:
   dir_name = "ppo-nn-" + "-".join([str(i) for i in arch])
-  policy_kwargs = dict(activation_fn=nn.ReLU,
+  policy_kwargs = dict(activation_fn=nn.Tanh,
                       net_arch=[dict(pi=arch, vf=arch)])
   for i in range(len(envs)):
     env = make_vec_env(envs[i], n_envs=8)
