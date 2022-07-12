8bba47692b5cccc825da2cefff5156b6b0142036
Gaëtan Serré <gaetan.serre@universite-paris-saclay.fr>
2022-07-13 00:46:43 +0200

    Add script to run experiments

---
+++ RL/launch_exp.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -x
+python run_trained_agents.py \
+--has_cuda 0 \
+--expe_name $1 \
+--path_agents ADPRL_paper \
+--safe_max_rho 0.95 \
+--limit_cs_margin 100 \
+--training_iter 1000000 2000000 3000000 4000000 5000000 6000000 7000000 8000000 9000000 10000000
