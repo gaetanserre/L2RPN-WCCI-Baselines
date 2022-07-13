1b3e516c1702d4acce8ef0925922fcf48d008c72
Gaëtan Serré <gaetan.serre@universite-paris-saclay.fr>
2022-07-13 19:02:39 +0200

    Update launch_exp script

--- RL/launch_exp.sh
+++ RL/launch_exp.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 set -x
+
 python run_trained_agents.py \
---has_cuda 0 \
---expe_name $1 \
---path_agents ADPRL_paper \
---safe_max_rho 0.95 \
---limit_cs_margin 100 \
---training_iter 1000000 2000000 3000000 4000000 5000000 6000000 7000000 8000000 9000000 10000000
---agent_name PPO_agent0_20220709_152030
+--has_cuda=0 \
+--safe_max_rho 0.5 0.6 0.7 0.8 0.9 0.95 1.0 1.1 1.2 \
+--limit_cs_margin 100. \
+--expe_name figure_7_1 \
+--nb_process 4 \
+--path_agents ADPRL_paper
+
+#--limit_cs_margin 40. 60. 80 100. 120. 140. 160. 180. \
+#--expe_name figure_7_2 \
+#--safe_max_rho 0.9 \
