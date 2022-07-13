51c724a1e943431974627a904dce176ccf4c78b5
Gaëtan Serré <gaetan.serre@universite-paris-saclay.fr>
2022-07-13 18:34:04 +0200

    Add launch scripts

--- RL/launch_exp.sh
+++ RL/launch_exp.sh
@@ -8,3 +8,4 @@ python run_trained_agents.py \
 --safe_max_rho 0.95 \
 --limit_cs_margin 100 \
 --training_iter 1000000 2000000 3000000 4000000 5000000 6000000 7000000 8000000 9000000 10000000
+--agent_name PPO_agent0_20220709_152030
